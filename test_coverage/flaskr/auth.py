967a1ce714707ac3683bde528a1d7f834cc58683
seitamuro <seitamuro0210@gmail.com>
2021-09-09 18:47:27 +0900

    add test coverage

--- test_coverage/flaskr/auth.py
+++ test_coverage/flaskr/auth.py
@@ -7,7 +7,7 @@ from flask import (
 import flask
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
