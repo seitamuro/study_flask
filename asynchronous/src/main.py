4f7df08a1d684731bef28267cf35cf5a176fc4d8
seitamuro <seitamuro0210@gmail.com>
2021-10-13 22:59:14 +0900

    add form

---
+++ asynchronous/src/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def hello_world(name):
+    return render_template("hello.html", name=name, method="URLパラメータ")
+
+@app.route("/param", methods=["GET", "POST"])
+def hello_world_with_parameter():
+    if request.method == "POST":
+        name = request.form["name"]
+    else:
+        name = request.args.get("name")
+
+    return render_template("hello.html", name=name, method=request.method)
+
+if __name__ == "__main__":
+    app.run()
